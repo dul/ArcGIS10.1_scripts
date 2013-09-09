96884cfcea7e69a45681f05664859d6903850fcb
maría dulce lauría <mariadulce.lau@gmail.com>
2013-09-09 15:02:42 -0300

    nuevo script 'Bookmarks to Excel'

---
+++ bookmarksToExcel.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import arcpy
+"""
+Summary
+"""
+
+
+try:
+
+	
+
+# manejo de excepciones
+except arcpy.ExecuteError:
+	print arcpy.GetMessages(2)
+	arcpy.AddError(arcpy.GetMessages(2))
+except Exception as e:
+	print e.args[0]
+	arcpy.AddError(e.args[0])
