22da8bdd5717c86a3a51dd77c91b76e2c1d5a43b
maría dulce lauría <mariadulce.lau@gmail.com>
2013-10-10 12:54:08 -0300

    nueva funcionalidad a exportar
    Transfiere código si no encuentra descripción

--- Exportar a shapefile con domain descriptions/exportarConDescripciones.py
+++ Exportar a shapefile con domain descriptions/exportarConDescripciones.py
@@ -13,10 +13,13 @@ def corresponde ( field_de_codigo, field_con_descripcion ):
 		return True
 	return False
 
-# Recibe una feature class y guarda en una lista todos los campos de código
-# que serán borrados luego. Devuelve la lista.
-def obtener_lista_de_fields_a_borrar (outName):
-	lista_de_fields_a_borrar = []
+# Recibe una feature class y guarda en un diccionario todos los campos de 
+# código que serán borrados luego. Las key del diccionario son los nombres
+# de los campos de descripciones, los valores son los nombres de los 
+# campos con código (a borrar)
+def obtener_dict_de_fields_a_borrar (outName):
+	
+	dict_de_fields_a_borrar = {} # { 'field_d':'field_c', ...}
 	
 	# Por cada campo que empiece con d_
 	for field_d in arcpy.ListFields(outName, 'd_*'):
@@ -25,10 +28,24 @@ def obtener_lista_de_fields_a_borrar (outName):
 			# Si coinciden los primeros 8 caracteres de field_c, lo
 			# guardo en la lista_de_fields_a_borrar
 			if (corresponde(field_c.name, field_d.name)):
-				lista_de_fields_a_borrar.append(field_c.name)
+				dict_de_fields_a_borrar[field_d.name] = field_c.name
 
-	return lista_de_fields_a_borrar
+	return dict_de_fields_a_borrar
 
+
+# Recibe una feature class y un diccionario con los pares: 
+# {'campo de descripciones':'campo de codigos',...}
+# Por cada fila de la feature class, si el valor en campo de descripciones 
+# es vacío (como guarda un caracter espacio ' ', considero vacío si largo
+# <=1 ), guardo ahi el valor que tiene el campo de códigos.
+def transferir_valores_si_null (outName, dict_de_fields_a_borrar):
+	rows = arcpy.UpdateCursor (outName)
+	for row in rows:
+		for key in dict_de_fields_a_borrar.iterkeys():
+			if len(row.getValue(key)) <= 1:
+				row.setValue(key, row.getValue(dict_de_fields_a_borrar[key]))
+				rows.updateRow(row)
+	
 
 
 
@@ -48,16 +65,18 @@ try:
 	#--- Limpiado de tablas de salida ---#
 	# Me muevo al directorio de salida
 	arcpy.env.workspace = outLocation
-	lista_de_fields_a_borrar = obtener_lista_de_fields_a_borrar(outName)
+	dict_de_fields_a_borrar = obtener_dict_de_fields_a_borrar(outName)
+	# Si hay valores en el campo de descripciones que quedaron vacios,
+	# copio los que tiene el campo a borrar
+	transferir_valores_si_null(outName, dict_de_fields_a_borrar)
 	# Elimino todos los fields de la lista
-	for field in lista_de_fields_a_borrar:
+	for field in dict_de_fields_a_borrar.values():
 		arcpy.DeleteField_management(outName, field)
 
 	#--- Restauracion de nombres de campos ---#
-	for field_c in lista_de_fields_a_borrar:
-		for field_d in arcpy.ListFields(outName, 'd_*'):
-			if (corresponde(field_c, field_d.name)):
-				renameFields_logica.renameFields(outName, field_d.name, field_c)
+	for key in dict_de_fields_a_borrar.iterkeys():
+		renameFields_logica.renameFields(outName, key, dict_de_fields_a_borrar[key])
+
 	
 
 	
